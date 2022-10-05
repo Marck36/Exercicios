bdac9b9ba39afe53992cfa01b0428574157f63e3
Marck36 <90698578+Marck36@users.noreply.github.com>
2022-10-04 23:28:58 -0300

    incrementando o exercicio de arquivos

--- Estrutura de dados/Arquivos/ExercicioArquivos.c
+++ Estrutura de dados/Arquivos/ExercicioArquivos.c
@@ -37,32 +37,188 @@
 // ✔ O código fonte do programa deve ser postado no Moodle, em local indicado pela
 // professora.
 
+int home()
+{
+    printf("################################################################################################################# \n");
+    printf("## TELA DE MENU \n");
+    printf("################################################################################################################# \n");
+    printf("1. Cadastrar marcas \n");
+    printf("2. Cadastrar carros \n");
+    printf("3.  \n");
+    printf("4. Sair \n");
+
+    int menu;
+    scanf("%i", &menu);
+    return menu;
+}
+
+char criarArquivoMarca(){
+    FILE *file;
+    file = fopen("Marcas.txt", "w");
+    fclose(file);
+}
+
+char criarArquivoCarros(){
+    FILE *file;
+    file = fopen("Carros.txt", "w");
+    fclose(file);
+}
+
+char cadastrarMarca(){
+    FILE *file;
+    file = fopen("Marcas.txt", "a");
+
+    if(file == NULL){
+        printf("Arquivo nao pode ser aberto ou nao existe!");
+        getchar();
+        exit(1);
+    }
+
+    int whileControl;
+    while(whileControl != 1){
+
+        char marca[20];
+        printf("Digite a marca:");
+        scanf("%s \n", &marca);
+        fputs(marca, file);
+
+        char marcaCodigo[20];
+        printf("Digite a marca:");
+        scanf("%s \n", &marcaCodigo);
+        fputs(marcaCodigo, file);
+
+        char marcaDescricao[100];
+        printf("Digite a marca:");
+        scanf("%s \n", &marcaDescricao);
+        fputs(marcaDescricao, file);
+
+        char control;
+        printf("Deseja inserir outra marca: Sim(s)/Nao(n)");
+        scanf("%s \n", &control);
+
+        if(strcpy(control, "n")){
+            whileControl = 1;
+            break;
+        }
+
+    }
+
+    fclose(file);
+}
+
+char cadastrarCarro(){
+    FILE *file;
+    file = fopen("Marcas.txt", "a");
+
+    if(file == NULL){
+        printf("Arquivo nao pode ser aberto ou nao existe!");
+        getchar();
+        exit(1);
+    }
+
+    int whileControl;
+    while(whileControl != 1){
+
+        char marca[20];
+        printf("Digite a marca:");
+        scanf("%s \n", &marca);
+        fputs(marca, file);
+
+        char anoFabricacao[4];
+        printf("Digite a marca:");
+        scanf("%s \n", &anoFabricacao);
+        fputs(anoFabricacao, file);
+
+        char placa[7];
+        printf("Digite a marca:");
+        scanf("%s \n", &placa);
+        fputs(placa, file);
+
+        char control;
+        printf("Deseja inserir outro carro: Sim(s)/Nao(n)");
+        scanf("%s \n", &control);
+
+        if(strcpy(control, "n")){
+            whileControl = 1;
+            break;
+        }
+
+    }
+
+    fclose(file);
+}
+
+char lerCarros(){
+    FILE *file;
+    file = fopen("Carros.txt", "a");
+
+    if(file == NULL){
+        printf("Arquivo nao pode ser aberto ou nao existe!");
+        getchar();
+        exit(1);
+    }
+
+    char carros[1000];
+
+    while(fgets(carros, 1000, file) != NULL){
+        printf("%s", carros);
+    }
+
+    fclose(file);
+}
+
+
 int main(){
 
-
-// return 0;
 // #################################################################################################################
 // ##    INICIANDO TELA DE MENU
 // #################################################################################################################
+int menu;
 
-// #################################################################################################################
-// ##    INICIANDO TELA DE CADASTRO
-// #################################################################################################################
+while(menu != 4){
 
+    menu = home();
 
-// #################################################################################################################
-// ##    INICIANDO TELA DE CADASTRO
-// #################################################################################################################
+    // #################################################################################################################
+    // ##    TELA DE CADASTRO DE MARCA
+    // #################################################################################################################
+    if (menu == 1)
+    {
+        int writeControl;
+        if(writeControl != 1){
+            criarArquivoMarca();
+            writeControl = 1;
+        }
+        cadastrarMarca();
+    }
 
+    // #################################################################################################################
+    // ##    TELA DE CADASTRO DE CARROS
+    // #################################################################################################################
+    if (menu == 2)
+    {
+        int writeControl;
+        if(writeControl != 1){
+            criarArquivoCarros();
+            writeControl = 1;
+        }
+        cadastrarCarro();
+    }
 
-// #################################################################################################################
-// ##    INICIANDO TELA DE EXECUCAO DA TRAVESSIA
-// #################################################################################################################
-
+    // #################################################################################################################
+    // ##    TELA DE LEITURA DE CARROS
+    // #################################################################################################################
+    if (menu == 3)
+    {
+        lerCarros();
+    }
+}
 
 // #################################################################################################################
 // ##    ENCERRANDO
 // #################################################################################################################
 
+printf("Saindo!...");
+return 0;
 
 }
